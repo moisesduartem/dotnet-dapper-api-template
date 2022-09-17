742f3c1ba442c788b30a511df562a4f2a6d1fc80
Moisés Maia <moisesduartem@gmail.com>
2022-09-17 19:48:21 -0300

    feat: register user command validations

--- src/RestApi.Application/V1/Aggregates/Users/Validators/RegisterUserCommandValidator.cs
+++ src/RestApi.Application/V1/Aggregates/Users/Validators/RegisterUserCommandValidator.cs
@@ -7,9 +7,27 @@ namespace RestApi.Application.V1.Aggregates.Users.Validators
     {
         public RegisterUserCommandValidator()
         {
-            RuleFor(x => x.Email).NotEmpty().EmailAddress();
-            RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
+            RuleFor(x => x.Email).NotEmpty()
+                                 .EmailAddress()
+                                 .MinimumLength(7)
+                                 .MaximumLength(60);
+
+            RuleFor(x => x.Password).NotEmpty()
+                                    .MinimumLength(6);
+
             RuleFor(x => x.PasswordConfirmation).Equal(x => x.Password);
+
+            RuleFor(x => x.FirstName).NotEmpty()
+                                     .MinimumLength(3)
+                                     .MaximumLength(18);
+
+            RuleFor(x => x.LastName).NotEmpty()
+                                    .MinimumLength(3)
+                                    .MaximumLength(25);
+
+            RuleFor(x => x.Birthdate).NotEmpty()
+                                       .Matches(@"^([0-9]){4}(-)([0-9]){2}(-)([0-9]){2}$")
+                                       .Length(10);
         }
     }
 }
